c38b4523d6cb0087ed3a6c7293fc1f9fd8e3b0bc
柄本広樹 <emotohiroki@emotohirokinoiMac.local>
2025-11-07 23:26:55 +0900

    markTestIncomplete pattern

--- app/tests/PersonTest.php
+++ app/tests/PersonTest.php
@@ -7,12 +7,17 @@ use App\Person;
 
 final class PersonTest extends TestCase
 {
-    public function testGetFullName(): void
+    public function test_苗字＋名前を取得できること(): void
     {
         $person = new Person();
         $person->setFirstName('John');
         $person->setSurname('Doe');
 
         $this->assertSame('John Doe', $person->getFullName());
+    }
+
+    public function test_未実装の項目(): void
+    {
+        $this->markTestIncomplete('このテストは未実装です。');
     }
 }
